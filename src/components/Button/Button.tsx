246dcbe5105462ca5059a877ce500d38b14e17be
benjdg16dev <benjdg16dev@gmail.com>
2023-03-12 21:17:15 -0400

    [feat] Reconfigure buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,28 +1,34 @@
-import { MouseEventHandler, ReactNode } from "react";
+import { MouseEventHandler, CSSProperties, ReactNode } from "react";
 
 import "./Button.css";
 
 interface IButton {
+  id?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   color?: "primary" | "secondary" | "warning" | "disable" | "success";
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
   children?: ReactNode;
+  styles?: CSSProperties;
 }
 
 const Button = ({
+  id,
   onClick,
   color = "primary",
   type = "button",
   disabled = false,
   children,
+  styles,
 }: IButton) => {
   return (
     <button
+      id={id}
       className={`btn ${color}`}
       type={type}
       disabled={disabled}
       onClick={onClick ? onClick : undefined}
+      style={styles}
     >
       {children}
     </button>
